9cf24934527d8b8abadf3527d36c561e9150a258
rafael2904 <rafa.grigorian@gmail.com>
2017-09-09 15:11:21 +0400

    comments1

--- Gaussian Elimination.py
+++ Gaussian Elimination.py
@@ -1,10 +1,15 @@
 import numpy as np
-# matrix init
+# the matrix
 arr = np.array([1, 2, 3, 2,
                 0, 4, 1, 1,
                 3, 5, 6, 0,
                 2, 0, 3, 4], np.int32)
+# size of the matrix
 n = 4
+# y is the right side of the linear equation after = sign
+y = np.array([3, 4, 10, 2], np.float16)
+y = y.reshape(n, 1)
+
 arr = arr.reshape(n, n)
 print(arr)
 print()
@@ -23,10 +28,6 @@ for i in range(0, n-1):
     for j in range(i+1, n):
         for k in range(0, n):
             arr[j][k] = arr[j][k]-arr[j][i]/arr[i][i]*arr[i][k]
-
-# y is the right side of the linear equation after = sign
-y = np.array([3, 4, 10, 2], np.float16)
-y = y.reshape(n, 1)
 print("matrix after pivotation")
 print(arr)
 
