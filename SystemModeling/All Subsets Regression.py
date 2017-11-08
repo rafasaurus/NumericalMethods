7f4085e31f184ebf61a0c76106f584e045638015
Rafael <rafa.grigorian@gmail.com>
2017-11-08 23:33:56 +0400

    computeRegY function build

--- SystemModeling/All Subsets Regression.py
+++ SystemModeling/All Subsets Regression.py
@@ -1,7 +1,7 @@
 #All Subsets Regression - Generalized Linear Models
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def sp(arr, n):
     s = 0
@@ -72,6 +72,15 @@ def COMPUTE_REGRESSION_Y_EX(yp, x1, x2, x3, n):  # stands for experimental
         y[3] += yp[i] * x3[i]
     return y
 
+def COMPUTE_REGRESSION_Y(yp,muffin, n, featureSize):
+    y = np.zeros(n, dtype=float)
+    for i in range(n):
+        if i==0:
+            y[i] = sumX(yp,featureSize)
+        else:
+            y[i] = sumXY(yp,muffin[i-1],featureSize)
+    return y
+
 df=pd.read_csv("Water Salinity and River Discharge.csv")
 #plt.scatter(df)
 #plt.show()
@@ -83,19 +92,25 @@ X2=df['X2']
 X3=df['X3']
 Y=df['Y']
 
+
+
 featureSize = df.__len__()
 featureRows = 3
+
+print("featureSize = ",featureSize)
 
 muffin = np.zeros(shape=(3, featureSize))
 muffin[0]=X1
 muffin[1]=X2
 muffin[2]=X3
+
 n=featureRows+1 #number of feature rows +1 for b0,b1,b2...
 X=COMPUTE_REGRESSION_X(muffin,featureRows,featureSize,n)
-vector = COMPUTE_REGRESSION_Y_EX(Y, muffin[0], muffin[1],muffin[2], featureSize)
+vector1 = COMPUTE_REGRESSION_Y_EX(Y, muffin[0], muffin[1],muffin[2], featureSize)
+vector = COMPUTE_REGRESSION_Y(Y, muffin,featureRows+1, featureSize)
 
 print(X)
-print()
+print(vector1)
 print(vector)
 
 inverse_arr = INVERSE_MATRIX(X, n)
