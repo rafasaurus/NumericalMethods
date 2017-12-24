79598681eb7da5dfcb35181e5e1756f3c9116c10
Rafael <rafa.grigorian@gmail.com>
2017-12-24 23:27:40 +0400

    IKT

--- AppliedInformationTheory/IKT.py
+++ AppliedInformationTheory/IKT.py
@@ -6,14 +6,16 @@ arr = np.zeros(shape=(19,19),dtype=int)
 compareZeroMatrix = np.zeros(shape=(19,19),dtype=int)
 zeros=arr
 #x = np.array([1, 1, 2, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 16, 17, 18, 19], np.float)
-x = np.array([1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 16, 17, 18, 19], np.float)
-y = np.array([8, 11, 13, 12, 16, 5, 14, 6, 15, 7, 19, 9, 0, 9, 19, 7, 16, 18, 14, 17, 0, 0, 0, 0], np.float)
+x = np.array([1, 1,  2,  2,  3,  3, 4,  4, 5,  6, 7,  8, 9, 10,11,12, 13, 14, 15, 16,16,17,18,19], np.int)
+y = np.array([8, 11, 13, 12, 16, 5, 14, 6, 15, 7, 19, 9, 0, 9, 19, 7, 16, 18, 14, 17, 0, 0, 0, 0], np.int)# 24 elements
 x_temp=9
 y_temp=0
+
 for i in range(19):
     for j in range(19):
-        for k in range(19):
-            if y[k]-1 !=0:
+        for k in range(24):
+            if y[k]-1 !=0 and y[k]!=0:
+                #if ((int(y[k]-1)==19 and int(x[k])-1)==19):
                 arr[int(x[k])-1][int(y[k]-1)]=1
 
 
@@ -21,19 +23,23 @@ for i in range(19):
 print(arr)
 print()
 
-global_arr=arr
+global_arr = np.zeros(shape=(19,19),dtype=int)
+global_arr = arr.copy()
 matrix_sum = np.zeros(shape=(19,19),dtype=int)
 N = 0
 while(np.array_equal(global_arr,compareZeroMatrix)==False):
     matrix_sum += global_arr
-    global_arr=np.matmul(global_arr,global_arr)
-    print("boolean check = ", np.array_equal(global_arr,compareZeroMatrix))
+    global_arr=np.matmul(global_arr,arr)
     N += 1
+    print("n=", N)
+    print("boolean check = ", np.array_equal(global_arr,compareZeroMatrix))
+    print(global_arr )
+
 
     #print(global_arr)
-N+=1
+
 print("N=",N)
-print("-----հասանելիության մատռից-----")
+print("-----հասանելիության մատրից-----")
 print(matrix_sum)
 #print("debug=",global_arr)
 #global_arr=arr
